9af344d962d9692e0b5d0bd0bbb7acc22eb74d0b
Nikolaj Lindberg <nikolaj@stts.se>
2019-05-28 10:06:35 +0200

    readXMLFILE

--- xmlreader/xmlreader_test.go
+++ xmlreader/xmlreader_test.go
@@ -33,3 +33,19 @@ func TestUnmarshalXML(t *testing.T) {
 
 	//fmt.Printf("%#v\n", ldml.Rbnf.RulesetGrouping[0])
 }
+
+func TestReadXMLFile(t *testing.T) {
+	// depends on file sv.xml in current dir
+
+	ldml, err := readXMLFile("sv.xml")
+	if err != nil {
+		t.Errorf("Sob! : %v", err)
+	}
+
+	//fmt.Printf("%#v\n")
+
+	if w, g := "sv", ldml.Identity.Language.Attrtype; w != g {
+		t.Errorf("wanted %s got %s", w, g)
+	}
+
+}
