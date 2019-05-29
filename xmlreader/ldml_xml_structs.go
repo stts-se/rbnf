4cb3692207855f4039e6c3abf26ad4c2c56cf8fc
Nikolaj Lindberg <nikolaj@stts.se>
2019-05-29 14:17:22 +0200

    re-ordered structs

--- xmlreader/ldml_xml_structs.go
+++ xmlreader/ldml_xml_structs.go
@@ -6,10 +6,21 @@ import (
 
 // Structs generated from Unicode.org's  ICU/RBNF spellout XML rule file using Chidley, https://github.com/gnewton/chidley
 
+type Ldml struct {
+	XMLName  xml.Name  `xml:"ldml,omitempty" json:"ldml,omitempty"`
+	Identity *Identity `xml:"identity,omitempty" json:"identity,omitempty"`
+	Rbnf     *Rbnf     `xml:"rbnf,omitempty" json:"rbnf,omitempty"`
+}
+
 type Identity struct {
 	XMLName  xml.Name  `xml:"identity,omitempty" json:"identity,omitempty"`
 	Language *Language `xml:"language,omitempty" json:"language,omitempty"`
 	Version  *Version  `xml:"version,omitempty" json:"version,omitempty"`
+}
+
+type Version struct {
+	XMLName    xml.Name `xml:"version,omitempty" json:"version,omitempty"`
+	Attrnumber string   `xml:"number,attr"  json:",omitempty"`
 }
 
 type Language struct {
@@ -17,22 +28,15 @@ type Language struct {
 	Attrtype string   `xml:"type,attr"  json:",omitempty"`
 }
 
-type Ldml struct {
-	XMLName  xml.Name  `xml:"ldml,omitempty" json:"ldml,omitempty"`
-	Identity *Identity `xml:"identity,omitempty" json:"identity,omitempty"`
-	Rbnf     *Rbnf     `xml:"rbnf,omitempty" json:"rbnf,omitempty"`
-}
-
 type Rbnf struct {
 	XMLName         xml.Name           `xml:"rbnf,omitempty" json:"rbnf,omitempty"`
 	RulesetGrouping []*RulesetGrouping `xml:"rulesetGrouping,omitempty" json:"rulesetGrouping,omitempty"`
 }
 
-type Rbnfrule struct {
-	XMLName   xml.Name `xml:"rbnfrule,omitempty" json:"rbnfrule,omitempty"`
-	Attrradix string   `xml:"radix,attr"  json:",omitempty"`
-	Attrvalue string   `xml:"value,attr"  json:",omitempty"`
-	String    string   `xml:",chardata" json:",omitempty"`
+type RulesetGrouping struct {
+	XMLName  xml.Name   `xml:"rulesetGrouping,omitempty" json:"rulesetGrouping,omitempty"`
+	Attrtype string     `xml:"type,attr"  json:",omitempty"`
+	Ruleset  []*Ruleset `xml:"ruleset,omitempty" json:"ruleset,omitempty"`
 }
 
 type Ruleset struct {
@@ -42,13 +46,9 @@ type Ruleset struct {
 	Rbnfrule   []*Rbnfrule `xml:"rbnfrule,omitempty" json:"rbnfrule,omitempty"`
 }
 
-type RulesetGrouping struct {
-	XMLName  xml.Name   `xml:"rulesetGrouping,omitempty" json:"rulesetGrouping,omitempty"`
-	Attrtype string     `xml:"type,attr"  json:",omitempty"`
-	Ruleset  []*Ruleset `xml:"ruleset,omitempty" json:"ruleset,omitempty"`
-}
-
-type Version struct {
-	XMLName    xml.Name `xml:"version,omitempty" json:"version,omitempty"`
-	Attrnumber string   `xml:"number,attr"  json:",omitempty"`
+type Rbnfrule struct {
+	XMLName   xml.Name `xml:"rbnfrule,omitempty" json:"rbnfrule,omitempty"`
+	Attrradix string   `xml:"radix,attr"  json:",omitempty"`
+	Attrvalue string   `xml:"value,attr"  json:",omitempty"`
+	String    string   `xml:",chardata" json:",omitempty"`
 }
