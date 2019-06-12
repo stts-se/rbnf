8caf22f633ad4b6e684bd73e81da070a605eaaf1
Hanna Lindgren <han.lind@gmail.com>
2019-06-12 16:59:37 +0200

    r

---
+++ comp-icu4j-vs-rbnfgo/batch_run_cardinals.sh
@@ -0,0 +1,49 @@
+set -e
+
+### DEPENDENCIES #################
+#
+# 1) https://github.com/unicode-org/icu.git
+#  $ git clone https://github.com/unicode-org/icu.git
+#  $ cd icu/icu4j
+#  $ ant
+#  => icu4j.jar
+#
+# 2) github.com/HannaLindgren/go-utils/scripts/compare_line_by_line/compare_line_by_line.go
+#  $ git clone https://github.com/HannaLindgren/go-utils/
+#  $ cd go-utils/scripts/compare_line_by_line
+#  $ go build
+#  => compare_line_by_line
+#
+##################################
+
+mkdir -p output
+
+icu4j_jar=~/progz/icu/icu4j/icu4j.jar
+
+#numsfile=nums_1_to_100k
+numsfile=nums_1_to_1M
+
+langs="en sv fr de da ja ar sk cs fi en_IN hu it ta ru"
+
+# not working: th
+
+for lang in $langs; do
+    echo "=== PROCESSING $lang ... " 1>&2
+    outgo="output/${numsfile}_out_${lang}_rbnfgo.txt"
+    outicu4j="output/${numsfile}_out_${lang}_icu4j.txt"
+
+    # rule set name
+    ruleset="spellout-numbering"
+    if [ $lang == "da" ]; then
+	ruleset="spellout-cardinal-common"
+    fi
+    echo "Rule set: $ruleset" 1>&2
+
+    
+    time cat ${numsfile}.txt | scala -cp ${icu4j_jar} batch_run_icu4j.scala ${lang} >| $outicu4j
+    time cat ${numsfile}.txt | go run ../cmd/spellout/spellout.go -r $ruleset https://github.com/unicode-org/cldr/raw/master/common/rbnf/${lang}.xml >| $outgo
+
+    compare_line_by_line -q $outgo $outicu4j &> output/${lang}.diff
+    echo "=== DONE" 1>&2
+    echo "" 1>&2
+done;
---
+++ comp-icu4j-vs-rbnfgo/batch_run_icu4j.scala
@@ -0,0 +1,34 @@
+#!/bin/sh
+exec scala -savecompiled "$0" "$@"
+!#
+
+import scala.io.StdIn
+
+import com.ibm.icu.text.RuleBasedNumberFormat
+import com.ibm.icu.util.ULocale
+
+if (args.length < 1) {
+  Console.err.println("USAGE: scala batch_run_icu4j.scala <langcode> <numerals>")
+  Console.err.println("   OR: cat <numeralfiles> | scala batch_run_icu4j.scala <langcode>")
+  System.exit(1)
+}
+
+val rbnf = new RuleBasedNumberFormat(new ULocale(args(0)), RuleBasedNumberFormat.SPELLOUT)
+
+if (args.length>=2) {
+  for (i <- 1 until args.length) {
+    val s = args(i)
+    val n = s.toInt
+    val res = rbnf.format(n)
+    Console.out.println(s + "\t" + res)
+  }
+} else {
+  var s = ""
+  while ({s = StdIn.readLine(); s != null}) {
+    val n = s.toInt
+    val res = rbnf.format(n)
+    Console.out.println(s + "\t" + res)
+  }
+
+
+}
