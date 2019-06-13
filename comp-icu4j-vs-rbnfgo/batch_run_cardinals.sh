00c491e17b34ff0d9a51b3ce1c3d77109efd09aa
Hanna Lindgren <han.lind@gmail.com>
2019-06-13 11:30:23 +0200

    r

--- comp-icu4j-vs-rbnfgo/batch_run_cardinals.sh
+++ comp-icu4j-vs-rbnfgo/batch_run_cardinals.sh
@@ -23,11 +23,14 @@ icu4j_jar=~/progz/icu/icu4j/icu4j.jar
 #numsfile=nums_1_to_100k
 numsfile=nums_1_to_1M
 
-langs="en sv fr de da ja ar cs fi en_IN hu it ta"
-
+all_langs="af ak am ar az be bg bs ca ccp chr cs cy da de de_CH ee el en en_001 en_IN eo es es_419 et fa fa_AF ff fi fil fo fr fr_BE fr_CH ga he hi hr hu hy id is it ja ka kl km ko ky lb lo lrc lt lv mk ms mt my nb nl nn pl pt pt_PT qu ro root ru se sk sl sq sr sr_Latn sv sw ta th tr uk vi yue yue_Hans zh zh_Hant"
+tested_langs="en sv fr de da ja ar cs fi en_IN hu it ta"
 # not working: th (not sure why); ru (sin/plu); sk (sin/plu)
 
+langs=$all_langs
+
 for lang in $langs; do
+
     echo "=== PROCESSING $lang ... " 1>&2
     outgo="output/${numsfile}_out_${lang}_rbnfgo.txt"
     outicu4j="output/${numsfile}_out_${lang}_icu4j.txt"
@@ -46,4 +49,5 @@ for lang in $langs; do
     compare_line_by_line -q $outgo $outicu4j &> output/${lang}.diff
     echo "=== DONE" 1>&2
     echo "" 1>&2
+
 done;
