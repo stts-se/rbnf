f852d42038ebc5e4d31ff32244d745fecb4e1ac7
Hanna Lindgren <han.lind@gmail.com>
2019-06-13 11:37:29 +0200

    r

--- comp-icu4j-vs-rbnfgo/batch_run_cardinals.sh
+++ comp-icu4j-vs-rbnfgo/batch_run_cardinals.sh
@@ -16,12 +16,14 @@ set -e
 #
 ##################################
 
-mkdir -p output
 
 icu4j_jar=~/progz/icu/icu4j/icu4j.jar
-
+outdir="output"
 #numsfile=nums_1_to_100k
 numsfile=nums_1_to_1M
+
+mkdir -p $outdir
+
 
 all_langs="af ak am ar az be bg bs ca ccp chr cs cy da de de_CH ee el en en_001 en_IN eo es es_419 et fa fa_AF ff fi fil fo fr fr_BE fr_CH ga he hi hr hu hy id is it ja ka kl km ko ky lb lo lrc lt lv mk ms mt my nb nl nn pl pt pt_PT qu ro root ru se sk sl sq sr sr_Latn sv sw ta th tr uk vi yue yue_Hans zh zh_Hant"
 tested_langs="en sv fr de da ja ar cs fi en_IN hu it ta"
@@ -29,11 +31,16 @@ tested_langs="en sv fr de da ja ar cs fi en_IN hu it ta"
 
 langs=$all_langs
 
+echo "LANGS: $langs" 1>&2
+echo "OUTDIR: $outdir" 1>&2
+echo "NUMSFILE: $numsfile" 1>&2
+echo "" 1>&2
+
 for lang in $langs; do
 
     echo "=== PROCESSING $lang ... " 1>&2
-    outicu4j="output/${lang}_${numsfile}_icu4j.txt"
-    outgo="output/${lang}_${numsfile}_rbnfgo.txt"
+    outicu4j="$outdir/${lang}_${numsfile}_icu4j.txt"
+    outgo="$outdir/${lang}_${numsfile}_rbnfgo.txt"
 
     # rule set name
     ruleset="spellout-numbering"
@@ -45,7 +52,7 @@ for lang in $langs; do
     
     time cat ${numsfile}.txt | scala -cp ${icu4j_jar} batch_run_icu4j.scala ${lang} >| $outicu4j
     if time cat ${numsfile}.txt | go run ../cmd/spellout/spellout.go -r $ruleset https://github.com/unicode-org/cldr/raw/master/common/rbnf/${lang}.xml >| $outgo ; then
-	compare_line_by_line -q $outgo $outicu4j &> output/${lang}.diff
+	compare_line_by_line -q $outgo $outicu4j &> $outdir/${lang}.diff
 	echo "=== $lang DONE" 1>&2
     else
 	echo "=== $lang FAILED" 1>&2
